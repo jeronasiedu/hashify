6f10dec9c60c3953b21c4f50250269c38f1497d5
Jeron <norejudeisa@gmail.com>
2023-09-24 13:32:40 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express"
+import multer from "multer"
+import { encodeImageToBlurhash } from "./utils/encode.js"
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
+
+app.get("/", (_, res) => {
+  return res.json({
+    message: "We're working on it!",
+  })
+})
+
+app.post("/generate-blurhash", async (req, res) => {
+  try {
+    const { imageUrl } = req.body
+    if (!imageUrl) {
+      return res.status(400).json({
+        error: "imageUrl is required in the request body",
+        recommendation:
+          "Please provide an image url or use the /generate-blurhash-from-file endpoint",
+      })
+    }
+    const blurHash = await encodeImageToBlurhash(imageUrl)
+    res.json({ blurHash })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: "An internal server error occurred" })
+  }
+})
+
+app.post("/generate-blurhash-from-file", upload.single("image"), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: "No file uploaded" })
+    }
+    if (!req.file.mimetype.includes("image")) {
+      return res.status(400).json({
+        error: "File must be an image",
+        recommendation: "Please provide an image url or use the /generate-blurhash endpoint",
+      })
+    }
+
+    const blurHash = await encodeImageToBlurhash(req.file.buffer, true)
+
+    res.json({ blurHash })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: "An internal server error occurred" })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
---
+++ utils/encode.js
@@ -0,0 +1,28 @@
+import { encode } from "blurhash"
+import sharp from "sharp"
+
+export const encodeImageToBlurhash = (image, isBuffer = false) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      let buffer
+      if (isBuffer) {
+        buffer = image
+      } else {
+        const response = await fetch(image)
+        if (!response.ok) {
+          return reject(new Error("Failed to fetch the image"))
+        }
+        buffer = await response.buffer()
+      }
+      sharp(buffer)
+        .raw()
+        .ensureAlpha()
+        .resize(32, 32, { fit: "inside" })
+        .toBuffer((err, buffer, { width, height }) => {
+          if (err) return reject(err)
+          resolve(encode(new Uint8ClampedArray(buffer), width, height, 4, 4))
+        })
+    } catch (error) {
+      reject(error)
+    }
+  })
